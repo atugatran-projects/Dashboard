f70cb43094a71c5399902b3c6b0e9715434a224f
Atul <atul22g2468@gmail.com>
2024-09-16 17:56:21 +0530

    some changs

--- pages/common/config.php
+++ pages/common/config.php
@@ -1,5 +1,5 @@
 <?php 
-define('SITE_URL','https://atul22g.alwaysdata.net/Dashboard');
-// define('SITE_URL','http://localhost/Atul/dashboard/');
+// define('SITE_URL','https://atul22g.alwaysdata.net/Dashboard/');
+define('SITE_URL','http://localhost/coders/dashboard/');
 define ('INV_ASSETS',SITE_URL.'assets');
 ?>
--- pages/database/dbconnect.php
+++ pages/database/dbconnect.php
@@ -1,24 +1,24 @@
 <?php 
 
 // Database configuration for localhost
-// $dbHost     = "localhost";  
-// $dbUsername = "root"; 
-// $dbPassword = ""; 
-// $dbName     = "atul22g_Dashboard"; 
+$dbHost     = "localhost";  
+$dbUsername = "root"; 
+$dbPassword = ""; 
+$dbName     = "atul22g_dashboard"; 
 
 // Connect to the Database  for alwaysdata
-$dbHost = "mysql-atul22g.alwaysdata.net";
-$dbUsername = "atul22g";
-$dbPassword = "RInnXFrPkhLqPJ8xbT6515Pk6BzKJPqCNjPnhcxV17dtwSLh9t";
-$dbName = "atul22g_Dashboard";
+// $dbHost = "mysql-atul22g.alwaysdata.net";
+// $dbUsername = "atul22g";
+// $dbPassword = "RInnXFrPkhLqPJ8xbT6515Pk6BzKJPqCNjPnhcxV17dtwSLh9t";
+// $dbName = "atul22g_dashboard";
 
 // Create database connection 
 $conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
 
 // Check connection 
 
-// if ($conn) {
-//     echo '<script>alert("Connection Success!")</script>';
-// } else {
-//     echo '<script>alert("Connection Failed!")</script>';
-// }
+if ($conn) {
+    echo '<script>alert("Connection Success!")</script>';
+} else {
+    echo '<script>alert("Connection Failed!")</script>';
+}
